5fa15e41b5f9eb6a1273ba9e5e7fb5220bf75a29
Carlos Javier Aceves Perez <ciap1382@gmail.com>
2023-12-09 22:20:22 -0600

    Terminar componente TarjetaPelicula con lineas 12 a 15

--- moviesite/src/Componentes/Site/Contenido/TarjetaPelicula.js
+++ moviesite/src/Componentes/Site/Contenido/TarjetaPelicula.js
@@ -3,11 +3,16 @@ const TarjetaPelicula = (props) => {
   props.pelicula.genre_ids.forEach(genero => {
     generos += genero + '';
   });
+  console.log(props);
     return (
     <>
       <div id="props.pelicula.id" className={"cardpeli" + generos}>
         <img src={"https://image.tmdb.org/t/p/w500" + props.pelicula.poster_path} alt="titulo"/>;
         <h1>{props.pelicula.title}</h1>
+        <div className="cardpelidescrip">
+          <p>Fecha: {props.pelicula.release_date}</p>
+          <p>Descripción: {props.pelicula.overview.substring(0, 40)}<a href="#">{" "}...Ver mas<></></a></p>
+        </div>
       </div>
     </>
   );
